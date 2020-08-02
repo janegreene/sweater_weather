1c23e6507fecb9f38d0944a1837bc837c647ee0c
Jane Greene <janegreenecasa@gmail.com>
2020-08-02 16:59:11 -0600

    added vcrs to tests

--- spec/requests/api/v1/forecast_spec.rb
+++ spec/requests/api/v1/forecast_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 describe "Forecast API" do
   it "send a forecast" do
-    get api_v1_forecast_index_path(params: { location: "denver, co" })
+    VCR.use_cassette('forecast_denver_co') do
+      get api_v1_forecast_index_path(params: { location: "denver, co" })
 
-    expect(response).to be_successful
-    expect(response.status).to eq(200)
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
 
-    forecast = JSON.parse(response.body, symbolize_names: true)
-    expect(forecast).to be_a(Hash)
+      forecast = JSON.parse(response.body, symbolize_names: true)
+      expect(forecast).to be_a(Hash)
+    end
   end
 end
--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -2,15 +2,18 @@ require 'rails_helper'
 
 describe "WeatherService" do
   it "#fetch_weather" do
-    latitude = 39.738453
-    longitude = -104.984853
-    service = WeatherService.new
-    response = service.fetch_weather(latitude, longitude)
+    # WebMock.allow_net_connect!
+    VCR.use_cassette('weather_for_denver_co') do
+      latitude = 39.738453
+      longitude = -104.984853
+      service = WeatherService.new
+      response = service.fetch_weather(latitude, longitude)
 
-    expect(response).to be_a(Hash)
-    expect(response[:current]).to be_a(Hash)
-    expect(response[:daily]).to be_a(Array)
-    expect(response[:daily].length).to eq(8)
-    expect(response[:hourly].length).to eq(48)
+      expect(response).to be_a(Hash)
+      expect(response[:current]).to be_a(Hash)
+      expect(response[:daily]).to be_a(Array)
+      expect(response[:daily].length).to eq(8)
+      expect(response[:hourly].length).to eq(48)
+    end
   end
 end
