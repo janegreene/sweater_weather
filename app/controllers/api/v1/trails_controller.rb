a039143334480ca1b9a4ed16a13317e1270a2f36
Jane Greene <janegreenecasa@gmail.com>
2020-08-03 11:10:25 -0600

    trail poro in progress

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -1,15 +1,22 @@
 class Api::V1::TrailsController < ApplicationController
   def index
     location = params[:location]
+    lat_long_json = MapquestService.new.get_lat_long(location)
+    lat = lat_long_json[:results][0][:locations][0][:latLng][:lat]
+    long = lat_long_json[:results][0][:locations][0][:latLng][:lng]
+
 
        conn = Faraday.new(url: "https://www.hikingproject.com") do |faraday|
          faraday.params["key"] = ENV['hiking_key']
-         faraday.params["lat"] = 40.0274
-         faraday.params["lon"] = -105.2519
+         faraday.params["lat"] = lat
+         faraday.params["lon"] = long
          faraday.params["limit"] = 10
        end
 
     response = conn.get("/data/get-trails")
-       require "pry"; binding.pry
+    trails = JSON.parse(response.body, symbolize_names: true)
+    forecast = ForecastFacade.new.get_weather(location)
+    Trail.new(trails)
+       # require "pry"; binding.pry
   end
 end
---
+++ app/poros/trail.rb
@@ -0,0 +1,6 @@
+class Trail
+  def initialize(trails_json)
+    require "pry"; binding.pry
+    @argument = argument
+  end
+end
