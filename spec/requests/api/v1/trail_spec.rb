665083b055ea4d9fc3bd478c825e00167c901670
Jane Greene <janegreenecasa@gmail.com>
2020-08-03 09:18:09 -0600

    added basic test for trail endpoint

---
+++ spec/requests/api/v1/trail_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "Trail API" do
+  it "sends trail info" do
+    # VCR.use_cassette('forecast_denver_co') do
+      get api_v1_trail_index_path(params: { location: "denver, co" })
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      forecast = JSON.parse(response.body, symbolize_names: true)
+      expect(forecast).to be_a(Hash)
+    # end
+  end
+
+
+# https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=
