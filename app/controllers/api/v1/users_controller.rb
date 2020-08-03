b015aaba97e3f139fae9581ec342a47f47020f93
Jane Greene <janegreenecasa@gmail.com>
2020-08-03 15:16:34 -0600

    commented out error until test is unskipped

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::UsersController < ApplicationController
   def create
     if params[:password] == params[:password_confirmation]
       user = User.create(user_params)
-      # require "pry"; binding.pry
       render json: UserSerializer.new(user), status: :created
     # else
     #   render :json => { status: 400, :errors => user.errors.full_messages }, status: 400
